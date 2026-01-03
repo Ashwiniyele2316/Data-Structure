84c315925d8f4ee95338ccfd57fe66f39406d8ab
Ashwiniyele2316 <ashwiniyele9@gmail.com>
2026-01-04 00:20:08 +0530

    Create Program49_4.c

---
+++ Assignment /Assignment 49/Program49_4.c
@@ -0,0 +1,262 @@
+/*
+| 11 | -> | 21 | -> | 51 | -> NULL
+Number of nodes are : 3
+| 11 | -> | 21 | -> | 51 | -> | 101 | -> | 111 | -> | 121 | -> NULL
+Number of nodes are : 6
+| 21 | -> | 51 | -> | 101 | -> | 111 | -> | 121 | -> NULL
+Number of nodes are : 5
+| 21 | -> | 51 | -> | 101 | -> | 111 | -> NULL
+Number of nodes are : 4
+| 21 | -> | 51 | -> | 105 | -> | 101 | -> | 111 | -> NULL
+Number of nodes are : 5
+| 21 | -> | 51 | -> | 101 | -> | 111 | -> NULL
+Number of nodes are : 4
+
+Total elements divisible by 5 are : 3
+
+
+
+*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }   
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    PNODE temp = *first;
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }   
+    else
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+}
+
+void DeleteFirst(PPNODE first)
+{
+    PNODE temp = *first;
+    if(*first == NULL) return;
+    
+    *first = (*first)->next;
+    free(temp);
+}
+
+void DeleteLast(PPNODE first)
+{
+    PNODE temp = *first;
+    if(*first == NULL) return;
+    
+    if((*first)->next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else
+    {
+        while(temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        free(temp->next);
+        temp->next = NULL;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d | -> ", first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    while(first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+    return iCount;
+}
+
+void InsertAtPos(PPNODE first, int no, int pos)
+{
+    int iSize = Count(*first);
+    PNODE newn = NULL;
+    PNODE temp = *first;
+
+    if((pos < 1) || (pos > iSize + 1))
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    if(pos == 1)
+    {
+        InsertFirst(first, no);
+    }
+    else if(pos == iSize + 1)
+    {
+        InsertLast(first, no);
+    }
+    else
+    {
+        newn = (PNODE)malloc(sizeof(NODE));
+        newn->data = no;
+        for(int i = 1; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        newn->next = temp->next;
+        temp->next = newn;
+    }
+}
+
+void DeleteAtPos(PPNODE first, int pos)
+{
+    int iSize = Count(*first);
+    PNODE temp = *first;
+    PNODE target = NULL;
+
+    if((pos < 1) || (pos > iSize))
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    if(pos == 1)
+    {
+        DeleteFirst(first);
+    }
+    else if(pos == iSize)
+    {
+        DeleteLast(first);
+    }
+    else
+    {
+        for(int i = 1; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        target = temp->next;
+        temp->next = target->next;
+        free(target);
+    }
+}
+
+int CountDivByFive(PNODE Head)
+{
+    int iCount = 0;
+    while(Head != NULL)
+    {
+        if((Head->data % 5) == 0)
+        {
+            iCount++;
+        }
+        Head = Head->next;
+    }
+    return iCount;
+}
+
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 21);
+    InsertFirst(&head, 11);
+    
+    Display(head);
+    
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+    
+    InsertLast(&head, 101);
+    InsertLast(&head, 111);
+    InsertLast(&head, 121);
+
+    Display(head);
+    
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteFirst(&head);
+    
+    Display(head);
+    
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteLast(&head);
+
+    Display(head);
+    
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    InsertAtPos(&head, 105, 3);
+
+    Display(head);
+    
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteAtPos(&head, 3);
+
+    Display(head);
+    
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    iRet = CountDivByFive(head);
+    printf("\nTotal elements divisible by 5 are : %d\n", iRet);
+
+    return 0;
+}
