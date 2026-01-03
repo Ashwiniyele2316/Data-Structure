4106c6dc00295f5a8f2b5d9f150de6aa409174f7
Ashwiniyele2316 <ashwiniyele9@gmail.com>
2026-01-03 23:43:55 +0530

    Create Program46_3.c
    This program implements a singly linked list with operations such as insertion, deletion, insertion/deletion at a specific position, display, and counting nodes. It also includes an IsEmpty function to check whether the linked list is empty or contains elements after performing all operations

---
+++ Assignment /Assignment 46/Program46_3.c
@@ -0,0 +1,247 @@
+/*
+| 11 | -> | 21 | -> | 51 | -> NULL
+Number of nodes are : 3
+| 11 | -> | 21 | -> | 51 | -> | 101 | -> | 111 | -> | 121 | -> NULL
+Number of nodes are : 6
+| 21 | -> | 51 | -> | 101 | -> | 111 | -> | 121 | -> NULL
+Number of nodes are : 5
+| 21 | -> | 51 | -> | 101 | -> | 111 | -> NULL
+Number of nodes are : 4
+| 21 | -> | 51 | -> | 105 | -> | 101 | -> | 111 | -> NULL
+Number of nodes are : 5
+| 21 | -> | 51 | -> | 101 | -> | 111 | -> NULL
+Number of nodes are : 4
+The list contains elements
+*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }   
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    PNODE temp = *first;
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }   
+    else
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+}
+
+void DeleteFirst(PPNODE first)
+{
+    PNODE temp = *first;
+    if(*first == NULL) 
+    {
+        return;
+    }
+    *first = (*first)->next;
+    free(temp);
+}
+
+void DeleteLast(PPNODE first)
+{
+    PNODE temp = *first;
+    if(*first == NULL) 
+    {
+        return;
+    }
+    if((*first)->next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else
+    {
+        while(temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        free(temp->next);
+        temp->next = NULL;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d | -> ", first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    while(first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+    return iCount;
+}
+void InsertAtPos(PPNODE first, int no, int pos)
+{
+    int iSize = Count(*first);
+    PNODE newn = NULL;
+    PNODE temp = *first;
+
+    if((pos < 1) || (pos > iSize + 1))
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    if(pos == 1)
+    {
+        InsertFirst(first, no);
+    }
+    else if(pos == iSize + 1)
+    {
+        InsertLast(first, no);
+    }
+    else
+    {
+        newn = (PNODE)malloc(sizeof(NODE));
+        newn->data = no;
+        for(int i = 1; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        newn->next = temp->next;
+        temp->next = newn;
+    }
+}
+void DeleteAtPos(PPNODE first, int pos)
+{
+    int iSize = Count(*first);
+    PNODE temp = *first;
+    PNODE target = NULL;
+
+    if((pos < 1) || (pos > iSize))
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    if(pos == 1)
+    {
+        DeleteFirst(first);
+    }
+    else if(pos == iSize)
+    {
+        DeleteLast(first);
+    }
+    else
+    {
+        for(int i = 1; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        target = temp->next;
+        temp->next = target->next;
+        free(target);
+    }
+}
+
+bool IsEmpty(PNODE Head)
+{
+    if(Head == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+    bool bRet = false;
+
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 21);
+    InsertFirst(&head, 11);
+    Display(head);
+    printf("Number of nodes are : %d\n", Count(head));
+
+    InsertLast(&head, 101);
+    InsertLast(&head, 111);
+    InsertLast(&head, 121);
+    Display(head);
+    printf("Number of nodes are : %d\n", Count(head));
+
+    DeleteFirst(&head);
+    Display(head);
+    printf("Number of nodes are : %d\n", Count(head));
+
+    DeleteLast(&head);
+    Display(head);
+    printf("Number of nodes are : %d\n", Count(head));
+
+    InsertAtPos(&head, 105, 3);
+    Display(head);
+    printf("Number of nodes are : %d\n", Count(head));
+
+    DeleteAtPos(&head, 3);
+    Display(head);
+    printf("Number of nodes are : %d\n", Count(head));
+
+    bRet = IsEmpty(head);
+    if(bRet == true)
+    {
+        printf("The list is empty\n");
+    }
+    else
+    {
+        printf("The list contains elements\n");
+    }
+
+    return 0;
+}
