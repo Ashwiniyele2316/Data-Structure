db511883f50df56d6aa8e5b90c5e23a8be6e1983
Ashwiniyele2316 <ashwiniyele9@gmail.com>
2026-01-04 11:27:18 +0530

    Create Program50_2.c
    This program implements a **singly linked list** in C supporting insertion, deletion, display, and counting of nodes, including position-based operations.
    It also calculates the **average of all node values** and displays the elements that are **greater than the average** of the linked list.

---
+++ Assignment /Assignment 50/Program50_2.c
@@ -0,0 +1,264 @@
+/*
+| 11 | -> | 21 | -> | 51 | -> NULL
+Number of nodes are : 3
+| 11 | -> | 21 | -> | 51 | -> | 101 | -> | 111 | -> | 121 | -> NULL
+Number of nodes are : 6
+| 21 | -> | 51 | -> | 101 | -> | 111 | -> | 121 | -> NULL
+| 21 | -> | 51 | -> | 101 | -> | 111 | -> NULL
+| 21 | -> | 51 | -> | 105 | -> | 101 | -> | 111 | -> NULL
+| 21 | -> | 51 | -> | 101 | -> | 111 | -> NULL
+Average is : 71.00
+Elements greater than average are : 101  111  
+
+
+*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }   
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    PNODE temp = *first;
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }   
+    else
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+}
+
+void DeleteFirst(PPNODE first)
+{
+    PNODE temp = *first;
+    if(*first == NULL) return;
+    
+    *first = (*first)->next;
+    free(temp);
+}
+
+void DeleteLast(PPNODE first)
+{
+    PNODE temp = *first;
+    if(*first == NULL) return;
+    
+    if((*first)->next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else
+    {
+        while(temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        free(temp->next);
+        temp->next = NULL;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d | -> ", first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    while(first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+    return iCount;
+}
+
+
+void InsertAtPos(PPNODE first, int no, int pos)
+{
+    int iSize = Count(*first);
+    PNODE newn = NULL;
+    PNODE temp = *first;
+
+    if((pos < 1) || (pos > iSize + 1))
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    if(pos == 1)
+    {
+        InsertFirst(first, no);
+    }
+    else if(pos == iSize + 1)
+    {
+        InsertLast(first, no);
+    }
+    else
+    {
+        newn = (PNODE)malloc(sizeof(NODE));
+        newn->data = no;
+        for(int i = 1; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        newn->next = temp->next;
+        temp->next = newn;
+    }
+}
+
+void DeleteAtPos(PPNODE first, int pos)
+{
+    int iSize = Count(*first);
+    PNODE temp = *first;
+    PNODE target = NULL;
+
+    if((pos < 1) || (pos > iSize))
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    if(pos == 1)
+    {
+        DeleteFirst(first);
+    }
+    else if(pos == iSize)
+    {
+        DeleteLast(first);
+    }
+    else
+    {
+        for(int i = 1; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        target = temp->next;
+        temp->next = target->next;
+        free(target);
+    }
+}
+
+void DisplayGreaterThanAvg(PNODE Head)
+{
+    int iSum = 0;
+    int iCnt = 0;
+    float fAvg = 0.0f;
+    PNODE temp = Head;
+
+    if(Head == NULL)
+    {
+        printf("List is empty.\n");
+        return;
+    }
+
+    while(temp != NULL)
+    {
+        iSum = iSum + temp->data;
+        iCnt++;
+        temp = temp->next;
+    }
+
+    fAvg = (float)iSum / (float)iCnt;
+    printf("Average is : %.2f\n", fAvg);
+
+  
+    printf("Elements greater than average are : ");
+    temp = Head;
+    while(temp != NULL)
+    {
+        if(temp->data > fAvg)
+        {
+            printf("%d  ", temp->data);
+        }
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 21);
+    InsertFirst(&head, 11);
+    
+    Display(head);
+    
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+    
+    InsertLast(&head, 101);
+    InsertLast(&head, 111);
+    InsertLast(&head, 121);
+
+    Display(head);
+    
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteFirst(&head);
+    Display(head);
+    
+    DeleteLast(&head);
+    Display(head);
+
+    InsertAtPos(&head, 105, 3);
+    Display(head);
+
+    DeleteAtPos(&head, 3);
+    Display(head);
+
+    DisplayGreaterThanAvg(head);
+
+    return 0;
+}
